9b2576b8f4c38474977329acbb0364ebd068ae0a
N.O.W <Weicholdo02@gmail.com>
2022-11-11 13:32:20 +0100

    Interaction Script Test

--- Assets/Scripts/Interact_Translate.cs
+++ Assets/Scripts/Interact_Translate.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Interact_Translate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public bool Interact;
+
+    private int i;
+    private Stone_Moving s_moving;
+    private bool[] USO = new bool[1];
+    /// <summary>
+    /// 0 = Stone_Moving
+    /// 
+    /// 
+    /// </summary>
+    private void Start()
     {
-        
+        if (GetComponent<Stone_Moving>())
+        {
+            s_moving = GetComponent<Stone_Moving>();
+            i = 0;
+            USO[i] = true;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Translate_Interact()
     {
-        
+        if (USO[i] == true && Interact)
+        {
+            switch (i)
+            {
+                case 0:
+                    s_moving.Stone_movable = true;
+                    break;
+                case 1:
+                    //Action
+                    break;
+            }
+        }
+        else if (USO[i] == true && !Interact)
+        {           
+            switch (i)
+            {
+                case 0:
+                    s_moving.Stone_movable = false;
+                    break;
+                case 1:
+                    //Action
+                    break;
+            }
+        }
+        Interact = false;
     }
+
+    private void Update() => Translate_Interact();
 }
