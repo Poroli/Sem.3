ace3df7daebf815ceb65bb88417d5c470087d6c7
DK <darian.kletzander@gmail.com>
2023-02-02 10:19:37 +0100

    MenuInteraction v.1.2

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+[System.Serializable]
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField][Range(0, 4)] private int functionID;
+    /// <summary>
+    /// 0 = NewGame
+    /// 1 = LoadGame
+    /// 2 = StartDemo
+    /// 3 = ExitGame
+    /// 4 = OpenOptions
+    /// </summary>
+    private SaveFunctions saveFunctions;
+
+    public void whichFunctionCalled() 
+    {
+        switch (functionID) 
+        {
+            case 0:
+                NewGame();
+                break;
+            case 1:
+                LoadGame();
+                break;
+            case 2:
+                StartDemo();
+                break;
+            case 3:
+                ExitGame();
+                break;
+            case 4:
+                OpenOptions();
+                break;
+        }
+    }
 
     private void NewGame()
     {
+        saveFunctions.NewGame();
         SceneManager.LoadScene("Level_1");
     }
     private void LoadGame()
@@ -25,5 +57,9 @@ public class MainMenu : MonoBehaviour
     private void OpenOptions()
     {
 
+    }
+    private void Start()
+    { 
+        saveFunctions = FindObjectOfType<SaveFunctions>();   
     }
 }
